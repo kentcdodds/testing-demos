ff14fa0de79479a90df808802d715ae961c02588
Kent C. Dodds <kent+github@doddsfamily.us>
2017-05-11 08:58:06 -0600

    Update arrayify.js

--- src/__tests__/arrayify.js
+++ src/__tests__/arrayify.js
@@ -1,17 +1,17 @@
 const arrayify = require('../arrayify')
 
-test('arrayify returns an empty array when given nothing', () => {
+test('returns an empty array when given nothing', () => {
   const result = arrayify()
   expect(result).toEqual([])
 })
 
-test(`arrayify returns the array-ed version of what it's given`, () => {
+test('returns the array-ed version of what it is given', () => {
   const input = 'hi there'
   const result = arrayify(input)
   expect(result).toEqual([input])
 })
 
-test(`arrayify returns the array if it's given an array`, () => {
+test('returns the array if it is given an array', () => {
   const input = [1, 2, 3]
   const result = arrayify(input)
   expect(result).toEqual(input)
